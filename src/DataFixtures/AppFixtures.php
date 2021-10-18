0ba6007128f44326c31e74fec37c03925dfcd747
Jordan <Berti@epse.be>
2021-10-18 09:46:42 +0200

    mise en place des fixtures et slug

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,40 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Ad;
+use Faker\Factory;
+use Cocur\Slugify\Slugify;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+
+        $faker = Factory::create('fr_FR');
+        $slugify = new Slugify();
+
+        for($i = 1; $i <= 30; $i++)
+        {
+            $ad = new Ad();
+            $title = $faker->sentence();
+            $slug = $slugify->slugify($title);
+            $coverImage = $faker->imageUrl(1000,350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>'.join('</p><p>',$faker->paragraphs(5)).'</p>';
+
+            $ad->setTitle($title)
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
+                ->setPrice(rand(40,200))
+                ->setRooms(rand(1,5));
+
+            $manager->persist($ad);
+
+        }
+
         // $product = new Product();
         // $manager->persist($product);
 
