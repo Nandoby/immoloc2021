5a003d650abdaa8e57254b51a01d30bcfcc1a19a
Boderio <boderiotrigan@hotmail.com>
2021-10-20 09:40:17 +0200

    cours 4 - form étape 1

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -24,6 +24,30 @@ class AdController extends AbstractController
         ]);
     }
 
+
+    /**
+     * Permet de créer une annonce
+     * @Route("/ads/new", name="ads_create")
+     * @return void
+     */
+    public function create(){
+
+        $ad = new Ad();
+        $form = $this->createFormBuilder($ad)
+                    ->add('title')
+                    ->add('introduction')
+                    ->add('content')
+                    ->add('rooms')
+                    ->add('price')
+                    ->getForm();
+
+
+        return $this->render("ad/new.html.twig",[
+            'form' => $form->createView()
+        ]);
+    }
+
+
     /**
      * Permet d'afficher une seule annonce
      * @Route("/ads/{slug}", name="ads_show")
@@ -39,4 +63,6 @@ class AdController extends AbstractController
             'ad' => $ad
         ]);
     }
+
+    
 }
